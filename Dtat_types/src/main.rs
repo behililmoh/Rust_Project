50c5d2fb21a50c963f9ffcf0123220c446766073
mohamed90460 <mbeh90460@gmail.comgit config --global user.name mohamed90460git config user.namegit config --global user.email mbeh90460@gmail.com>
2025-08-02 02:00:39 +0100

    add2

--- Dtat_types/src/main.rs
+++ Dtat_types/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+fn main(){
+  let celsius :f64 = 25.0 ;//type annotation required for floating-point calculations
+  let fahrenheit = (celsius *9.0 / 5.0)+32.0;
+  println!("{}°C est égal à {}°F", celsius, fahrenheit );
+
+  //- f64 is a scalar type for floating-point numbers .
+  //- Here, Rust automatically infers the type of fahrenheit.
+  
+  let _guess: u32 = "42".parse().expect("Not a number!");
+ 
+ //Integer  
+let x= -42;
+let y: u8 = 255;
+println!("the value of x: {} and the value of  y is:{}",x ,y);
 }
---
+++ Dtat_types/target/debug/deps/Dtat_types-feba1f04d22d0518.d
@@ -0,0 +1,5 @@
+C:\Users\sbaop\Rust_Project\Dtat_types\target\debug\deps\Dtat_types-feba1f04d22d0518.d: src\main.rs
+
+C:\Users\sbaop\Rust_Project\Dtat_types\target\debug\deps\libDtat_types-feba1f04d22d0518.rmeta: src\main.rs
+
+src\main.rs:
