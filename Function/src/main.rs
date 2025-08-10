3c18650bc311eed12500b7f2e173e2ae3b0ef986
mohamed <behilil.mohamed@gmail.com>
2025-08-10 17:15:19 +0200

    f2

--- Function/src/main.rs
+++ Function/src/main.rs
@@ -3,12 +3,16 @@ fn main(){
   
   another_function();
   another_function2(5);
+ anther_function3(5, 'h');
 }
 
  fn another_function(){
     println!("ANOTHER FUNCYION");
  }
+ fn anther_function3(value:i32,unit_label:char){
+  println!("The measurement is :{}{}",value,unit_label);
 
+ }
  fn another_function2(x:i32){
    println!("the value of x is :{}",x);
  }
