f1dfaab6bffd1c6ec049b215d0d31e484866df0f
behililmoh <moh685763@gmail.com>
2025-09-07 18:43:23 +0200

    //println!({:#?}, user1);

--- Instantiating_Structs/src/main.rs
+++ Instantiating_Structs/src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 //difining and instanting Structure
  struct User {
          active: bool,
@@ -6,12 +7,25 @@
          sign_in_count: u64,
 }
 fn main(){
+    
     let user1 = User {
         active:true,
         username:String::from("usernam"),
         email: String::from("usernam@exmple.org"),
         sign_in_count: 1,
-                     }; 
+                     };
+            
+    let mut user2 = User {
+        active:true,
+        username:String::from("usernam"),
+        email: String::from("usernam@exmple.org"),
+        sign_in_count: 1,
+                     };
+                //how to change the value in the email
+    user2.email = String::from("anotheremail@examplke.com");
 
-
+//Printing the structs using debug formatting
+println!("{:#?}", user1);
+ println!("{:#?}", user2);
+  
           }
